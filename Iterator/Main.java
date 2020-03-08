e7b7b2640688ad165d532b4cd26a1777ec5a8e2c
mpanska <47063770+mpanska@users.noreply.github.com>
2020-03-08 13:28:54 +0100

    Added test for showFailure(), resized list

--- Iterator/Main.java
+++ Iterator/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        ListOfStudents list = new ListOfStudents(3);
+        ListOfStudents list = new ListOfStudents(4);
         list.initList();
 
         list.printList();
@@ -13,5 +13,8 @@ public class Main {
         list.setGradeByIndex(5.25,250891);
         list.setGradeByIndex(0,236506);
         list.printList();
+        System.out.println();
+
+        list.showFailure();
     }
 }
